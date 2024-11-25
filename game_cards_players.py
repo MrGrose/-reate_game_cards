d346f7c6acc719cdd5584af06689a4b751928488
MrGrose <147311692+MrGrose@users.noreply.github.com>
2024-11-25 10:58:41 +0300

    Add files via upload
    Исправил ошибки.

--- game_cards_players.py
+++ game_cards_players.py
@@ -13,7 +13,7 @@ LETTERS_MAPPING = ast.literal_eval(
 )
 
 
-def chang_skill_format() -> list[str]:
+def change_skill_format() -> list[str]:
     runic_skills = [
         ''.join(LETTERS_MAPPING.get(char, char) for char in skill)
         for skill in HERO_SKILLS
@@ -22,30 +22,39 @@ def chang_skill_format() -> list[str]:
 
 
 def generate_character_attributes() -> dict[str, int]:
-    character_attribute = {
-        atr: random.randint(3, 18)
-        for atr in ["strength", "agility", "endurance", "intelligence", "luck"]
-        }
+    character_attribute = {atr: random.randint(3, 18) for atr in [
+        "strength",
+        "agility",
+        "endurance",
+        "intelligence",
+        "luck",
+        ]
+    }
     return character_attribute
 
 
-def cards_attributes() -> tuple[list[str], str, int]:
+def cards_attributes() -> tuple[list[str], int, int, int, int, int, list[str]]:
     people_characteristic = [
-        FAKE.first_name(), FAKE.last_name(), FAKE.job(), FAKE.city()
-        ]
+        FAKE.first_name(),
+        FAKE.last_name(),
+        FAKE.job(),
+        FAKE.city(),
+    ]
     attributes = generate_character_attributes()
-    skills = random.sample(chang_skill_format(), 3)
+    skills = random.sample(change_skill_format(), 3)
     return (
-        people_characteristic, attributes['strength'],
-        attributes['agility'], attributes['endurance'],
-        attributes['intelligence'], attributes['luck'],
-        skills
-        )
+        people_characteristic,
+        attributes['strength'],
+        attributes['agility'],
+        attributes['endurance'],
+        attributes['intelligence'],
+        attributes['luck'],
+        skills,
+    )
 
 
 def main() -> None:
-    if not os.path.isdir('Card_players'):
-        os.makedirs('Card_players')
+    os.makedirs('Card_players', exist_ok=True)
     for i in range(1, 11):
         card_info = cards_attributes()
         card_player = {
@@ -60,7 +69,7 @@ def main() -> None:
             'luck': card_info[5],
             'skill_1': card_info[6][0],
             'skill_2': card_info[6][1],
-            'skill_3': card_info[6][2]
+            'skill_3': card_info[6][2],
         }
         card_name = f'Card_players_{i}.svg'
         file_operations.render_template(
